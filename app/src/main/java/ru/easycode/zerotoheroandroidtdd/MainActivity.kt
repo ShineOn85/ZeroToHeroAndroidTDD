dd99d8ab5319a54f0675b413f8ed297a50da3178
AbsoluteLee <shineon85@yandex.ru>
2023-12-07 23:42:28 +0500

    test008 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,32 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var isGone = false
+    private lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button: Button = findViewById(R.id.hideButton)
+        textView = findViewById(R.id.titleTextView)
+        button.setOnClickListener{
+            textView.visibility = View.GONE
+            isGone = true
+        }
+        savedInstanceState?.let {
+            isGone = savedInstanceState.getBoolean("isGone")
+            if (isGone)
+                textView.visibility = View.GONE
+        }
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean("isGone", isGone)
     }
 }
